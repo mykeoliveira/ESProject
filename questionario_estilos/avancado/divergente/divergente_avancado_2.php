ca47ccc7663c289796e6400243da6f502c8c32b5
Luiz Henrique Romero <neo_luiz_@hotmail.com>
2020-11-27 22:46:45 -0300

    Update divergente_avancado_2.php
    Uma opção para voltar à questão anterior foi adicionada.

--- questionario_estilos/avancado/divergente/divergente_avancado_2.php
+++ questionario_estilos/avancado/divergente/divergente_avancado_2.php
@@ -922,7 +922,8 @@ function TypeChars(Chars){
 <br />
 
 <button id="CheckButton2" class="FuncButton" onmouseover="FuncBtnOver(this)" onfocus="FuncBtnOver(this)" onmouseout="FuncBtnOut(this)" onblur="FuncBtnOut(this)" onmousedown="FuncBtnDown(this)" onmouseup="FuncBtnOut(this)" onclick="CheckAnswers()">&nbsp;Verificar&nbsp;</button>
-<a href="divergente_avancado_3.php"> Próximo </a>
+<a href="divergente_avancado_1.php"> Anterior </a>
+		    <a href="divergente_avancado_3.php"> Próximo </a>
 
 
 </div>
