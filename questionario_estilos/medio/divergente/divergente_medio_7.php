313f45039b9d68914a45f5f89d308341c29c94ed
Luiz Henrique Romero <neo_luiz_@hotmail.com>
2020-11-28 00:52:37 -0300

    Update divergente_medio_7.php
    Foi adicionada uma opção para voltar à questão anterior.

--- questionario_estilos/medio/divergente/divergente_medio_7.php
+++ questionario_estilos/medio/divergente/divergente_medio_7.php
@@ -822,6 +822,7 @@ function CheckAnswers(){
 </div>
 <br>
 <button id="CheckButton2" class="FuncButton" onmouseover="FuncBtnOver(this)" onfocus="FuncBtnOver(this)" onmouseout="FuncBtnOut(this)" onblur="FuncBtnOut(this)" onmousedown="FuncBtnDown(this)" onmouseup="FuncBtnOut(this)" onclick="CheckAnswers()">&nbsp;Verificar&nbsp;</button>
+<a href="divergente_medio_6.php"> Anterior </a>
 <a href="divergente_medio_8.php"> Próximo </a>
 </div>
 
