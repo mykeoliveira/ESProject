d5426d73939b34dec074f4abb2c84a16b12a173e
Luiz Henrique Romero <neo_luiz_@hotmail.com>
2020-11-27 23:00:13 -0300

    Update impulsivo_avancado_5.php
    Um botão para voltar à questão anterior foi adicionado.

--- questionario_estilos/avancado/impulsivo/impulsivo_avancado_5.php
+++ questionario_estilos/avancado/impulsivo/impulsivo_avancado_5.php
@@ -908,7 +908,7 @@ function TypeChars(Chars){
 <br />
 
 <button id="CheckButton2" class="FuncButton" onmouseover="FuncBtnOver(this)" onfocus="FuncBtnOver(this)" onmouseout="FuncBtnOut(this)" onblur="FuncBtnOut(this)" onmousedown="FuncBtnDown(this)" onmouseup="FuncBtnOut(this)" onclick="CheckAnswers()">&nbsp;Verificar&nbsp;</button>
-
+<a href="divergente_avancado_4.php"> Anterior </a>
 <a href="impulsivo_avancado_6.php"> Próximo </a>
 
 </div>
